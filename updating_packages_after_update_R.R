3403cac6cf4854476cbb1d87817b274aa4849981
Kate Miller <kathryn_miller@nps.gov>
2023-07-17 16:06:18 -0400

    new scripts

--- updating_packages_after_update_R.R
+++ updating_packages_after_update_R.R
@@ -2,8 +2,12 @@
 oldRlibpath <- "C:/Program Files/R/R-4.0/library"
 oldRdocpath <- "C:/Users/KMMiller/Documents/R/win-library/4.0"
 
+packs = as.data.frame(installed.packages(.libPaths()[1]), stringsAsFactors = F)
+?installed.packages
+
 pkg_list <- c(unname(installed.packages(lib.loc = oldRlibpath)[, "Package"]),
-              unname(installed.packages(lib.loc = oldRlibpath)[, "Package"]))
+              unname(installed.packages(lib.loc = oldRdocpath)[, "Package"]))
+pkg_list
 
 install.packages(pkg_list)
 .libPaths() #"C:/Users/KMMiller/Documents/R/win-library/4.0" "C:/Program Files/R/R-4.0.3/library"
